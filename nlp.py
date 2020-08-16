b96cb83ebee7bca0ccbdb5871ff899500534c833
Rahul Pulidindi <rahul025@uw.edu>
2020-08-16 08:31:45 -0400

    more changes

--- nlp.py
+++ nlp.py
@@ -5,12 +5,11 @@ from google.cloud.language import types
 # export GOOGLE_APPLICATION_CREDENTIALS="/Users/rahulpulidindi/Downloads/Miranda-60c57eeb597d.json"
 
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ""
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/rahulpulidindi/Downloads/Miranda-60c57eeb597d.json"
 
 # Instantiates a client
 client = language.LanguageServiceClient()
 
-<<<<<<< HEAD
 def analyze_text(textFile) :
     with open(textFile) as f:
         text = f.read()
@@ -35,30 +34,7 @@ def analyze_text(textFile) :
             print('Metadata: {}'.format(entity.metadata))
             print('Salience: {}'.format(entity.salience))
 
-analyze_text('policeinteraction.txt')
+analyze_text('/Users/rahulpulidindi/Downloads/policeinteraction.txt')
 
-# # The text to analyze
-# with open('policeinteraction.txt') as f:
-#     text = f.read()
-# document = types.Document(
-#     content=text,
-#     type=enums.Document.Type.PLAIN_TEXT)
-
-# # Detects the sentiment of the text
-# sentiment = client.analyze_sentiment(document=document).document_sentiment
-
-# print('Text: {}'.format(text))
-# print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
-
-# response = client.analyze_entities(
-#     document=document,
-#     encoding_type='UTF32',
-# )
-
-# for entity in response.entities:
-#     print('Name: {0}'.format(entity.name))
-#     print('Type: {0}'.format(entity.type))
-#     print('Metadata: {0}'.format(entity.metadata))
-#     print('Salience: {0}'.format(entity.salience))
 
 
