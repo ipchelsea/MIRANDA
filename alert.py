179d85f1b4f15169bef8ba5a3c91b82a7fffb849
Rahul Pulidindi <rahul025@uw.edu>
2020-08-16 12:09:45 -0400

    Add alert

---
+++ alert.py
@@ -0,0 +1,14 @@
+from twilio.rest import Client
+
+account_sid = 'AC151263fea7e30db4d04a2a67b1d7fb03'
+auth_token = 'b41cdb088b5a1e75a7cf710f990f5c33'
+
+def alert_family():
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+                    .create(
+                         body="ALERT: Ada Lovelace is being pulled over by Officer Tony Shales of the Houston Police Department at Gulf Freeway, Houston, TX 77017 at 9:13 PM. TAP to call.",
+                         from_='+12513060440',
+                         to='+14256289296'
+                    )
