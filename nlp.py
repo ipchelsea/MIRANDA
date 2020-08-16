ea730b437d5d6d36ef7d11910da20ed3c2f015a9
Rahul Pulidindi <rahul025@uw.edu>
2020-08-16 08:22:29 -0400

    Some changes

--- nlp.py
+++ nlp.py
@@ -2,31 +2,58 @@
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
+# export GOOGLE_APPLICATION_CREDENTIALS="/Users/rahulpulidindi/Downloads/Miranda-60c57eeb597d.json"
 
 # Instantiates a client
 client = language.LanguageServiceClient()
 
-# The text to analyze
-with open('policeinteraction.txt') as f:
-    text = f.read()
-document = types.Document(
-    content=text,
-    type=enums.Document.Type.PLAIN_TEXT)
+def analyze_text(textFile) :
+    with open(textFile) as f:
+        text = f.read()
+        document = types.Document(
+            content=text,
+            type=enums.Document.Type.PLAIN_TEXT
+        )
 
-# Detects the sentiment of the text
-sentiment = client.analyze_sentiment(document=document).document_sentiment
+        sentiment = client.analyze_sentiment(document=document).document_sentiment
 
-print('Text: {}'.format(text))
-print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+        print('Text: {}'.format(text))
+        print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
 
-response = client.analyze_entities(
-    document=document,
-    encoding_type='UTF32',
-)
+        response = client.analyze_entities(
+            document=document, 
+            encoding_type='UTF32',
+        )
 
-for entity in response.entities:
-    print('Name: {0}'.format(entity.name))
-    print('Type: {0}'.format(entity.type))
-    print('Metadata: {0}'.format(entity.metadata))
-    print('Salience: {0}'.format(entity.salience))
+        for entity in response.entities:
+            print('Name: {}'.format(entity.name))
+            print('Type: {}'.format(entity.type))
+            print('Metadata: {}'.format(entity.metadata))
+            print('Salience: {}'.format(entity.salience))
+
+analyze_text('policeinteraction.txt')
+
+# # The text to analyze
+# with open('policeinteraction.txt') as f:
+#     text = f.read()
+# document = types.Document(
+#     content=text,
+#     type=enums.Document.Type.PLAIN_TEXT)
+
+# # Detects the sentiment of the text
+# sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+# print('Text: {}'.format(text))
+# print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+
+# response = client.analyze_entities(
+#     document=document,
+#     encoding_type='UTF32',
+# )
+
+# for entity in response.entities:
+#     print('Name: {0}'.format(entity.name))
+#     print('Type: {0}'.format(entity.type))
+#     print('Metadata: {0}'.format(entity.metadata))
+#     print('Salience: {0}'.format(entity.salience))
 
